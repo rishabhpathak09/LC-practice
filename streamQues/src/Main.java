eded8a456723e7cd9b9e7bcb1d5a46e8239efe11
Rishabh Pathak <rishabhpathak.ac@gmail.com>
2023-10-16 21:37:52 +0530

    distinct ele, starts with 1

--- streamQues/src/Main.java
+++ streamQues/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
-        Questns.evenNos(new int[]{1,2,3,4,5,6});
+//        Questns.evenNos(new int[]{1,2,3,4,5,6});
+//        Questns.startsWid1(new int[]{10,2,34,413,578,16});
+        Questns.distinctEles(new int[]{10,15,8,49,25,98,98,32,15});
         }
     }
--- streamQues/src/Questns.java
+++ streamQues/src/Questns.java
@@ -1,8 +1,29 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Questns {
     public static void evenNos(int[] arr){
         Arrays.stream(arr).boxed().filter(x->(x%2)==0).forEach(System.out::println);
+    }
+
+    protected static void startsWid1(int[] arr){
+        Arrays.stream(arr)
+                .boxed()
+                .filter(x ->
+                    x.toString().matches("1[0-9]*")
+                )
+                .forEach(System.out::println);
+    }
+
+    public static void distinctEles(int[] arr) {
+        Set<Integer> hash = new HashSet<Integer>();
+
+        Arrays.stream(arr)
+                .boxed()
+                .filter(hash::add)
+                .forEach(System.out::println);
     }
 }
