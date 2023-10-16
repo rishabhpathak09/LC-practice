53aa1f4cec51f800993022325982d3e027cddbf5
Rishabh Pathak <rishabhpathak.ac@gmail.com>
2023-10-17 01:06:29 +0530

    frequency of chars

--- streamQues/src/Questns.java
+++ streamQues/src/Questns.java
@@ -1,8 +1,7 @@
 import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -46,5 +45,65 @@ public class Questns {
                 .average()
                  .getAsDouble();
         System.out.println("Hi "+x);
+    }
+
+    public static void firstRepeated(String str) {
+        /*
+        Set s = new HashSet();
+
+        String res =
+                Stream.of(str.toLowerCase().split(""))
+                .filter(x -> !s.add(x))
+//                        .forEach(System.out::println);
+                .toList()
+                .get(0);
+
+         */
+        Integer res = str.chars()
+                .mapToObj(Character::toLowerCase)
+                .collect(Collectors.groupingBy(
+                                Function.identity(),
+                                LinkedHashMap::new,
+                                Collectors.counting()
+                        )
+                )
+                .entrySet()
+                .stream()
+                .filter(x -> x.getValue()>1L)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
+
+        System.out.println("firstRepeated: " +(char)res.intValue());
+    }
+
+    public static void sort(int[] ints) {
+    }
+
+    public static void concatStream(int[] ints, int[] ints1) {
+    }
+
+    public static void countWords(String[] strings) {
+    }
+
+    public static void nullableList(int[] ints) {
+    }
+
+    public static void countEach(String str) {
+        HashMap<String, Long> res = Stream.of(str.toLowerCase().split(""))
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        HashMap::new,
+                        Collectors.counting()
+                ));
+        res.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue()
+                )
+                .forEach(
+                        x->{
+                            System.out.println(x.getKey()+" = "+x.getValue());
+                        }
+                );
     }
 }
